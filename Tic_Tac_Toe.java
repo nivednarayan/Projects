b89945b4f544659fdef3c5fd374bb5bfb68e3a21
nivednarayan <90884258+nivednarayan@users.noreply.github.com>
2024-12-18 21:37:37 +0530

    Added 4 methods

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,5 +1,57 @@
 package projects;
-
+import java.util.*;
 public class Tic_Tac_Toe {
-
+	char board[][];
+	int player;
+	
+	public Tic_Tac_Toe() {
+		board = new board[3][3];
+		player = 1;
+		start();
+	}
+	
+	public void start() {
+		for(int i : board)
+			for(int j : board)
+				board[i][j] = '';
+	}
+	
+	public void print() {
+		for(int i : board) {
+			for(int j : board)
+				System.out.print(board[i][j] + " ");
+			System.out.println();
+		}
+	}
+	
+	public boolean moveValid(int r, int c) {
+		if( (r>=0&&r<3) && (c>=0&&c<3) && (board[r][c] == ''))
+			return true;
+		return false;
+	}
+	
+	public void move(int r, int c) {
+		if(moveValid(r,c)) {
+			if(player == 1)
+				board[r][c] = 'X';
+			else
+				board[r][c] = 'O';
+		}
+		else {
+			System.out.println("Invalid Move!.");
+			System.exit(0);
+		}	
+	}
+	
+	public boolean win() {
+		
+//		for(int i: board) {
+//			char k = board[][];
+//			for(int j: board) {
+//				if(board[ ][])
+//			}
+//		}
+		
+	}
+	
 }
