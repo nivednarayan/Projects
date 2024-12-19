7048a0f7eb6c2b9f0ac6c95dd2779f4dbb44cb20
nivednarayan <90884258+nivednarayan@users.noreply.github.com>
2024-12-19 13:09:46 +0530

    Updated Tic_Tac_Toe.java

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,57 +1,105 @@
-package projects;
+package projects.Projects;
 import java.util.*;
 public class Tic_Tac_Toe {
+	Scanner sc=new Scanner(System.in);
 	char board[][];
-	int player;
-	
+	boolean player;
 	public Tic_Tac_Toe() {
-		board = new board[3][3];
-		player = 1;
+		board = new char[3][3];
+		player = true;
 		start();
 	}
 	
 	public void start() {
-		for(int i : board)
-			for(int j : board)
-				board[i][j] = '';
+		for(int i=0;i<3;i++)
+			for(int j=0;j<3;j++)
+				board[i][j] = ' ';
 	}
 	
 	public void print() {
-		for(int i : board) {
-			for(int j : board)
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++)
 				System.out.print(board[i][j] + " ");
 			System.out.println();
 		}
 	}
 	
 	public boolean moveValid(int r, int c) {
-		if( (r>=0&&r<3) && (c>=0&&c<3) && (board[r][c] == ''))
+		if( (r>=0&&r<3) && (c>=0&&c<3) && (board[r][c] == ' '))
 			return true;
 		return false;
 	}
 	
 	public void move(int r, int c) {
 		if(moveValid(r,c)) {
-			if(player == 1)
+			if(player == true)
 				board[r][c] = 'X';
 			else
 				board[r][c] = 'O';
 		}
 		else {
-			System.out.println("Invalid Move!.");
-			System.exit(0);
+			System.out.println("Invalid Move!. Try again.");
 		}	
 	}
 	
 	public boolean win() {
 		
-//		for(int i: board) {
-//			char k = board[][];
-//			for(int j: board) {
-//				if(board[ ][])
-//			}
-//		}
+		for(int i=0;i<3;i++) {
+			if( (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) )
+				return true;
+			if( (board[0][i] == board[1][i]) && (board[1][i] == board[2][i]) )
+				return true;
+		}
 		
+		if( ( board[0][0] == board[1][1] ) && ( board[1][1] == board[2][2] ) )
+			return true;
+		if( ( board[0][2] == board[1][1] ) && ( board[1][1] == board[2][0] ) )
+			return true;
+		return false;
 	}
 	
+	public boolean full() {
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				if(board[i][j] == ' ')
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean draw() {
+		if(full() && !win())
+			return true;
+		return false;
+	}
+	
+	public void play() {
+		int r,c;
+		start();
+		while(!draw() && !win() ) {
+			print();
+			System.out.println("Enter next move of player " + (player ? 1 : 2));
+			r = sc.nextInt();
+			c = sc.nextInt();
+			move(r,c);
+			if( draw() ) {
+				print();
+				System.out.println("Match Drawn.");
+				break;
+			}
+			if( win() ) {
+				print();
+				System.out.println("Match won by player " + (player ? 1 : 2));
+				break;
+			}
+			player = !player;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Tic_Tac_Toe t = new Tic_Tac_Toe();
+		t.play();
+	}
+
 }
